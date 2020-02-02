1ec8b91c880784d0f2a8003ddcc226c7c979764d
James Swank <jmswank7@gmail.com>
2020-02-02 13:40:46 -0700

    initial commit

---
+++ navbar.py
@@ -0,0 +1,24 @@
+import dash_bootstrap_components as dbc
+
+
+def Navbar():
+  navbar = dbc.NavbarSimple(
+        children=[
+          dbc.NavItem(dbc.NavLink("Arctic Sea Ice Extent", href="/ice")),
+          dbc.DropdownMenu(
+              nav=True,
+              in_navbar=True,
+              label="Menu",
+              children=[
+                dbc.DropdownMenuItem("Arctic Sea Ice", href="/ice"),
+                dbc.DropdownMenuItem("Entry 2"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Entry 3"),
+                      ],
+                  ),
+                ],
+      brand="Home",
+      brand_href="/",
+      sticky="top",
+    )
+  return navbar
